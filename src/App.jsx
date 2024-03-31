2418dfb114cd0c1b1b4c3cccfb9cdd38df5d8476
Hironori Wada <hironori104@gmail.com>
2024-03-31 17:23:09 +0900

    done: ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,43 +3,64 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let [names, setNames] = useState([]);
-  const googleAddress = "https://google.com";
-
+  const [todos, setTodos] = useState([]);
   let [text, setText] = useState("");
 
-  function onClick() {
-    setNames([...names, "john" + new Date().toISOString()]);
+  function changeText(e) {
+    setText(e.target.value);
+  }
+  function onAdd() {
+    setTodos([...todos, text]);
+    setText("");
+  }
+  function onDelete(index) {
+    setTodos(todos.filter((_, i) => i !== index));
   }
 
   return (
     <>
-      {text}
-      {names.map((name, index) =>
-        index % 2 === 0 ? (
-          <div>
-            <a href={`${googleAddress}/search?q=スタイダーマン${name}`}>
-              {name}
-            </a>
+      <div style={{ display: "flex", gap: "8px" }}>
+        <input value={text} onChange={changeText}></input>
+        <button onClick={onAdd}>ADD</button>
+      </div>
+      {todos.map((todo, i) => (
+        <div
+          style={{
+            display: "flex",
+            gap: "8px",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <div style={{ display: "flex", gap: "8px", alignItems: "center" }}>
+            <input type="checkbox" />
+            <p>{todo}</p>
+            <p style={{ flexGrow: true }} />
           </div>
-        ) : (
           <div>
-            <a
-              style={{ color: "red" }}
-              href={`${googleAddress}/search?q=スタイダーマン${name}`}
+            <button
+              style={{ display: "block", padding: "4px" }}
+              onClick={() => onDelete(i)}
             >
-              {name}
-            </a>
+              <svg
+                style={{ width: "12px", height: "12px" }}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                onMouseOver={() => console.log("a")}
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                />
+              </svg>
+            </button>
           </div>
-        )
-      )}
-      <button onClick={onClick}>ADD</button>
-      <input
-        onChange={(e) => {
-          console.log("change " + e.target.value);
-          setText(e.target.value);
-        }}
-      ></input>
+        </div>
+      ))}
     </>
   );
 }
